5ff8a781d321712301e8bbb9f737436941fcf572
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2022-03-12 10:06:54 -0500

    Added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,10 +19,12 @@ import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
 import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
 import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
