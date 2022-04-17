92d4a4964acc026cc5e8099ee063c9772eb40a25
myin1 <44211285+myin1@users.noreply.github.com>
2022-04-17 11:40:45 -0400

    added form to career

--- src/app/careers/careers.component.ts
+++ src/app/careers/careers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-careers',
@@ -6,8 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./careers.component.scss']
 })
 export class CareersComponent implements OnInit {
+  selectedFile: File = null as any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  onClickSubmit(data: any) {
+    //alert("Entered data is: " + JSON.stringify(data))
+    this.http.post("http://localhost:8080/mail", data).subscribe(() => {
+      console.log('Data added successfully!')
+    }, (err) => {
+      console.log(err);
+    });
+
+  }
+
+  onFileSelected(file : any){
+    // this.selectedFile = file;
+    console.log(file)
+    this.selectedFile = <File>file.target.files[0];
+    console.log(this.selectedFile)
+  }
+
+  onUpload(){
+    const fileData = new FormData();
+    fileData.append('fileUpload', this.selectedFile, this.selectedFile.name);
+    console.log(fileData);
+    this.http.post("http://localhost:8080/upload", fileData) 
+    .subscribe(res => {
+      console.log(res);
+    });
+  }
 
   ngOnInit(): void {
   }
